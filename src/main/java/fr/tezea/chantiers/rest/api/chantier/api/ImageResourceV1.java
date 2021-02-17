9aadaa362232ec26baa890e3816365149ffe037a
14004306 <MotDePasse!>
2021-02-17 17:31:07 +0100

    Implementation de l'upload d'images

---
+++ src/main/java/fr/tezea/chantiers/rest/api/chantier/api/ImageResourceV1.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 TEZEA-Chantiers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package fr.tezea.chantiers.rest.api.chantier.api;
+
+import fr.tezea.chantiers.service.dto.chantier.ImageDTO;
+import java.io.IOException;
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/api/v1/image")
+public interface ImageResourceV1
+{
+    @GetMapping("/get/{id}")
+    @ResponseBody
+    public ResponseEntity<ImageDTO> getImageById(@PathVariable("id") long id);
+
+    @PostMapping("/add")
+    @ResponseBody
+    public ResponseEntity<URI> addImage(@RequestParam("image") MultipartFile image, Model model) throws IOException;
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> deleteImageById(@PathVariable("id") long id);
+}
