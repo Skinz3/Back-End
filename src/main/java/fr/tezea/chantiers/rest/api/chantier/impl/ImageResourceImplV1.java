4cb863a51565882cd4816595ed68d500003dc6af
14004306 <MotDePasse!>
2021-02-19 15:48:07 +0100

    Rolling back

--- src/main/java/fr/tezea/chantiers/rest/api/chantier/impl/ImageResourceImplV1.java
+++ src/main/java/fr/tezea/chantiers/rest/api/chantier/impl/ImageResourceImplV1.java
@@ -56,9 +56,8 @@ public class ImageResourceImplV1 implements ImageResourceV1
     @Override
     public ResponseEntity<URI> addImage(@RequestParam("image") MultipartFile image, Model model) throws IOException
     {
-    	long imageIndex = imageService.addImage(image);
-        URI location = URI.create(String.format("/get/%s", imageIndex));
-        return ResponseEntity.created(location).body(URI.create("{\"imageId\":"+ imageIndex));
+        URI location = URI.create(String.format("/get/%s", imageService.addImage(image)));
+        return ResponseEntity.created(location).build();
     }
 
     @Override
